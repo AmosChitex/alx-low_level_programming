aaf8d4ad0afd9aa1e20ea823f9ffbb8d6416086e
AmosChitex <chijiokeamos2@gmail.com>
2023-04-18 13:13:03 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,16 @@
+#include "sum.h"
+#include "sum.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = SUM(x, y);
+    printf("%d\n", s);
+    return (0);
+}
